6e20a32fc11c4cb05f09c5cabea48f1ac0bf176d
Tabita <122915607+tabita000@users.noreply.github.com>
2023-08-30 22:59:12 -0500

    Add files via upload

---
+++ VariablesTabitaS.playground/Pages/Safer Code in an Unexpected World.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+/*:
+ ## Safer Code in an Unexpected World
+
+ Changing the value of something should only be done deliberately. When writing a program, it should be clear what each piece of code is expected to do. If you make everything a variable, you or someone else might change a value either accidentally or on purpose. In any case the change can cause a problem somewhere down the line.
+ 
+ Consider this program for recording and calculating scores in a game:
+*/
+// Scores for each target
+var scoreForGreen = 5
+var scoreForRed = 10
+var scoreForGold = 20
+
+// Player scores
+var scoreForGary = 0
+var scoreForRob = 0
+
+// Game events here
+scoreForGary += scoreForRed
+scoreForGary += scoreForGreen
+scoreForGary += scoreForGold
+
+scoreForRob += scoreForRed
+scoreForRed += scoreForGreen
+scoreForRob += scoreForGold
+
+scoreForGary += scoreForRed
+scoreForGary += scoreForGreen
+scoreForGary += scoreForGold
+
+scoreForRob += scoreForRed
+scoreForRob += scoreForGreen
+scoreForRob += scoreForGold
+
+scoreForRob
+scoreForGary
+/*: 
+ - callout(Exercise): The program above has a problem. Each player hit the same targets, but at the end of the game, Rob has fewer points than Gary. Can you find the problem?\
+ _Hint: Try defining the target scores at the start of the program with `let` instead of `var`_
+*/
+// The problem is first set of scores for Rob 10 + 15 + 35//
+let scoreForGREEN = 5
+let scoreForRED = 10
+let scoreForGOLD = 20
+
+var scoreforGARY = 0
+var scoreforROB = 0
+
+scoreforGARY += scoreForRED
+scoreforGARY += scoreForGREEN
+scoreforGARY += scoreForGOLD
+
+scoreforROB += scoreForRED
+scoreforROB += scoreForGREEN
+scoreforROB += scoreForGOLD
+
+scoreforGARY += scoreForRED
+scoreforGARY += scoreForGREEN
+scoreforGARY += scoreForGOLD
+
+scoreforROB += scoreForRED
+scoreforROB += scoreForGREEN
+scoreforROB += scoreForGOLD
+
+scoreforROB
+scoreforGARY
+
+
+//: [Previous](@previous)  |  page 10 of 13  |  [Next: Wrapup](@next)
