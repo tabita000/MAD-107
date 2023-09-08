b2727eb2bb920f0c48adbbde706762c0f4924467
Tabita <122915607+tabita000@users.noreply.github.com>
2023-09-08 15:37:39 -0500

    Add files via upload
    Calculator TabitaS

---
+++ Calculator-TabitaS.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//Top Section variables
+var num1 = 2  //Could be any num. I'm using 2 and 4 for num1 and num2
+var num2 = 4
+
+//Middle Section defining operators
+let addition = num1 + num2
+let subtraction = num2 - num1
+let multiplication = num1 * num2
+let division = num2 / num1
+let percentage = num2 / num1 * 100
+
+
+//Bottom Section Calculations
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(percentage)
+
